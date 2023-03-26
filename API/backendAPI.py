e24d3a04f8aeaeee8eba493ff4aa52c2174a7859
Kristofer Buno <krisbuno@gmail.com>
2023-03-26 16:46:55 +0100

    api changes

--- API/backendAPI.py
+++ API/backendAPI.py
@@ -12,10 +12,10 @@ firebase_admin.initialize_app(cred)
 app = Flask(__name__)
 
 
-@app.route('/test', methods=['GET'])
+@app.route('/', methods=['GET'])
 def test():
     if request.method == "GET":
         return jsonify({"response": "Get Request Called"})
     
 if __name__ == '__main__':
-    app.run(debug=True, port=3030)
+    app.run(debug=True)
