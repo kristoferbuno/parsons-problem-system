3799d2b3724a6515ec4c11bdb7cad3e99cbbc1fb
Kristofer Buno <krisbuno@gmail.com>
2023-04-16 16:53:31 -0400

    Display problem data better on card

--- src/Problem.tsx
+++ src/Problem.tsx
@@ -25,6 +25,21 @@ class Problem {
         return this.title;
     }
 
+    ProblemData(): ReactElement {
+      let lines = []
+
+      for (let key in this.problem) {
+        lines.push(<Typography style={{backgroundColor: "whitesmoke", display: "block", fontFamily: "monospace"}}>
+          {this.problem[key]}
+        </Typography>)
+        lines.push(<br/>)
+      }
+
+      return <div>
+        {lines}
+      </div>
+    }
+
     ProblemCard(): ReactElement {
         return (
           <Card sx={{ minWidth: 275 }} key={this.id}>
@@ -35,12 +50,29 @@ class Problem {
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 {this.desc}
               </Typography>
-              <Typography variant="body2">
-                {this.problem}
-              </Typography>
+              {this.ProblemData()}
               <Button variant="outlined" href={'/solve/'+this.id}>Solve</Button>
               <span> </span>
               <Button variant="outlined" href={'/viewsolutions/'+this.id}>View Solutions List</Button>
+              <Typography sx={{ fontSize: 11 }} color="text.secondary" align={"right"}>
+                id: {this.id}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      }
+
+    InfoCard(): ReactElement {
+        return (
+          <Card sx={{ minWidth: 275 }} key={this.id}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {this.title}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {this.desc}
+              </Typography>
+              {this.ProblemData()}
               <Typography sx={{ fontSize: 11 }} color="text.secondary" align={"right"}>
                 id: {this.id}
               </Typography>
