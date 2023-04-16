42b8c908e1c34a9bb90cf2905116edc0831bf73a
Kristofer Buno <krisbuno@gmail.com>
2023-04-16 17:49:56 -0400

    Add solutiondata element to solution

--- src/Solution.tsx
+++ src/Solution.tsx
@@ -6,14 +6,14 @@ import { Button } from '@mui/material';
 
 class Solution {
     id: string;
-    problem_uid: string;
+    problemid: string;
     UFID: string;
     solution: string[];
     datetime: Moment;
     
-    constructor (id: string, problem_uid: string, UFID: string, solution: string[], datetime: Moment) {
+    constructor (id: string, problemid: string, UFID: string, solution: string[], datetime: Moment) {
         this.id = id;
-        this.problem_uid = problem_uid;
+        this.problemid = problemid;
         this.UFID = UFID;
         this.solution = solution;
         this.datetime = datetime;
@@ -23,6 +23,21 @@ class Solution {
       return this.UFID;
     }
 
+    SolutionData(): ReactElement {
+      let lines = []
+
+      for (let key in this.solution) {
+        lines.push(<Typography style={{backgroundColor: "whitesmoke", display: "block", fontFamily: "monospace"}}>
+          {this.solution[key]}
+        </Typography>)
+        lines.push(<br/>)
+      }
+
+      return <div>
+        {lines}
+      </div>
+    }
+
     // modeled similarly to ProblemCard, for use on SolutionListView
       // still need to add view for specific solution
     SolutionCard(): ReactElement {
@@ -30,10 +45,10 @@ class Solution {
           <Card sx={{ minWidth: 275 }} key={this.id}>
             <CardContent>
               <Typography variant="h5" component="div">
-                {this.UFID}
+                Submitted by {this.UFID}
               </Typography>
               <Typography variant="body2">
-                {this.solution}
+                {this.SolutionData()}
               </Typography>
               <Button variant="outlined" href={'/view/'+this.id}>View Solution</Button>
               <Typography sx={{ fontSize: 11 }} color="text.secondary" align={"right"}>
